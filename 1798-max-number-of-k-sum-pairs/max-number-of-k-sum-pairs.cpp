e36fe8a2c8a326491bc31f600f003fd1c89d86ba
Sowmya Varakala <66997833+sowmyavarakala@users.noreply.github.com>
2024-04-12 13:46:48 -0400

    Time: 139 ms (14.47%) | Memory: 76.4 MB (14.22%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
-	int maxOperations(vector<int>& arr, int k) {
-        sort(arr.begin(),arr.end());
-        int i = 0 ; int j = arr.size()-1;
-        int op =0;
-        while(i<j)
-        {
-            if(arr[i]+arr[j] == k)
-            {
-                i++;j--;
-                op ++;
-            }
-            else if(arr[i]+arr[j] < k)
-            {
-                i++;
-            }
-            else if(arr[i]+arr[j] > k)
-            {
-                j--;
-            }
-        }
-        return op;
-    }
+	int maxOperations(vector<int>& nums, int k) {
+		unordered_map<int, int> umap;
+		int ans = 0;
+		for(int i = 0; i < nums.size(); i++){
+			if(umap[k - nums[i]] > 0){
+			   umap[k - nums[i]]--;
+				ans++;
+			}
+			else{
+				umap[nums[i]]++;
+			}
+		}
+		return ans;
+	}
 };
