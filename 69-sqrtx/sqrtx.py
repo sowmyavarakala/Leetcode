af4c8cab191985ed1440a90a93730498737fd37d
Sowmya Varakala <66997833+sowmyavarakala@users.noreply.github.com>
2024-04-21 15:40:31 -0400

    Time: 43 ms (41.92%) | Memory: 16.6 MB (8.10%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -5,10 +5,10 @@ class Solution:
         left, right = 1, x
         while left <= right:
             mid = (left + right) // 2
-            if mid * mid == x:
+            if mid * mid <= x < (mid + 1) * (mid + 1):
                 return mid
             elif mid * mid < x:
                 left = mid + 1
             else:
                 right = mid - 1
-        return right
+        return left
