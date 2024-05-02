6effa9ca140665b5bcf9258123427f4e9cccc590
Sowmya Varakala <66997833+sowmyavarakala@users.noreply.github.com>
2024-05-02 19:45:36 -0400

    Time: 5 ms (24.80%) | Memory: 11.7 MB (97.24%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -15,10 +15,12 @@ public:
         if (p == NULL && q == NULL) {
             return true;
         }
-        if (p == NULL || q == NULL || p->val != q->val) {
+        if (p == NULL || q == NULL) {
             return false;
         }
-        
-    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        if (p->val == q->val) {
+            return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        }
+        return false;
     }
 };
