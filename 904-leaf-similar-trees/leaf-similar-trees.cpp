b30e3bd6b0131905c76f9c91334ad816a55b21ea
Sowmya Varakala <66997833+sowmyavarakala@users.noreply.github.com>
2024-05-05 00:19:20 -0400

    Time: 3 ms (60.95%) | Memory: 14.4 MB (33.01%) - LeetSync

--- 904-leaf-similar-trees/leaf-similar-trees.cpp
+++ 904-leaf-similar-trees/leaf-similar-trees.cpp
@@ -11,18 +11,20 @@
  */
 class Solution {
 public:
-    void dfs(TreeNode* node, vector<int>& tree){
-        if(node == nullptr) return;
-        if(node->left == nullptr && node->right == nullptr){
-            tree.push_back(node->val);
-        }
-        dfs(node->left,tree);
-        dfs(node->right,tree);
-    }
     bool leafSimilar(TreeNode* root1, TreeNode* root2) {
-        vector<int> tree1, tree2;
-        dfs(root1, tree1);
-        dfs(root2, tree2);
-        return tree1 == tree2;
+        stack<TreeNode*> s1 , s2;
+        s1.push(root1); s2.push(root2);
+        while (!s1.empty() && !s2.empty())
+            if (dfs(s1) != dfs(s2)) return false;
+        return s1.empty() && s2.empty();
+    }
+
+    int dfs(stack<TreeNode*>& s) {
+        while (true) {
+            TreeNode* node = s.top(); s.pop();
+            if (node->right) s.push(node->right);
+            if (node->left) s.push(node->left);
+            if (!node->left && !node->right) return node->val;
+        }
     }
 };
