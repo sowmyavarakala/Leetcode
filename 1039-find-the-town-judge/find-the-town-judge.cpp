35bd4f7ef90f41f27ada69ed153b30fb7e5745c5
Sowmya Varakala <66997833+sowmyavarakala@users.noreply.github.com>
2024-07-14 19:35:33 -0400

    Time: 145 ms (31.29%) | Memory: 72.7 MB (23.68%) - LeetSync

---
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        if (n == 1)
+        return trust.empty() ? 1 : -1;
+
+        vector<int> trusted_by(n+1,0);
+        vector<int> trusts(n+1,0);
+
+        for(auto t :  trust){
+            trusts[t[0]]++;
+            trusted_by[t[1]]++;
+        }
+        for( int i = 1; i <= n; i++){
+            if(trusts[i] == 0 && trusted_by[i] == n - 1){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
