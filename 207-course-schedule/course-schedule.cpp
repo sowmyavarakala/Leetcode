021f6da1f01f88d83e1d7dabe24840965c393d2d
Sowmya Varakala <66997833+sowmyavarakala@users.noreply.github.com>
2024-07-14 18:29:01 -0400

    Time: 17 ms (48.44%) | Memory: 17 MB (72.96%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,34 @@
+class Solution {
+private:
+    bool dfs(int course, const vector<vector<int>>& adj_list, vector<int>& state) {
+        if (state[course] == 1) return true;  
+        if (state[course] == 2) return false; 
+
+        state[course] = 1; 
+        for (int neighbor : adj_list[course]) {
+            if (dfs(neighbor, adj_list, state)) return true;
+        }
+        state[course] = 2;
+        return false;
+    }
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj_list(numCourses);
+        vector<int> state(numCourses, 0);
+
+        // Build the adjacency list
+        for (auto& p : prerequisites) {
+            adj_list[p[1]].push_back(p[0]);
+        }
+
+        // Check for cycles in all courses
+        for (int course = 0; course < numCourses; ++course) {
+            if (state[course] == 0 && dfs(course, adj_list, state)) {
+                return false;
+            }
+        }
+        
+        return true;
+        
+    }
+};
