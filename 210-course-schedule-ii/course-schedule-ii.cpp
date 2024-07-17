91ca01c9ed2c5b34f869c7a4cb089c8d4511023d
Sowmya Varakala <66997833+sowmyavarakala@users.noreply.github.com>
2024-07-17 19:29:55 -0400

    Time: 20 ms (25.77%) | Memory: 17.2 MB (68.28%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,42 +1,35 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>& adj_list, int course, vector<int>& state, stack<int>& order){
-        if(state[course] == 1) return true;
-        if(state[course] == 2) return false;
-
-        state[course] = 1;
-        for(auto neigh: adj_list[course]){
-            if(dfs(adj_list, neigh, state, order)){
-                return true;
-            }
-        }
-        state[course] = 2;
-        order.push(course);
-        return false;
-        
-    }
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        bool hasCycle = false;
         vector<vector<int>> adj_list(numCourses);
-        vector<int> state(numCourses, 0);
+        vector<int> indegree(numCourses, 0);
         stack<int> order;
 
         for(auto& p : prerequisites){
             adj_list[p[1]].push_back(p[0]);
+            indegree[p[0]]++;
         }
-
-        for(int course = 0; course < numCourses; course++){
-            if(state[course] == 0 && dfs(adj_list, course, state, order)){
-                hasCycle = true;
-                break;
+        vector<int> result;
+        for(int i = 0; i < numCourses; i++){
+            if(indegree[i] == 0){
+                order.push(i);
             }
         }
-        if(hasCycle) return {};
-        vector<int> result;
         while(!order.empty()){
-            result.push_back(order.top());
+            int course = order.top();
             order.pop();
+            result.push_back(course);
+
+            for(int neigh: adj_list[course]){
+                indegree[neigh]--;
+                if (indegree[neigh] == 0) {
+                    order.push(neigh);
+                }
+            }
         }
+        if(result.size() != numCourses) return {};
+        
+        
         return result;
     }
 };
