881343d4b87ea5fce41bca73faac2810dee17d13
Sowmya Varakala <66997833+sowmyavarakala@users.noreply.github.com>
2024-07-18 20:09:34 -0400

    Time: 52 ms (73.50%) | Memory: 38.3 MB (60.64%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,40 +1,19 @@
 class Solution {
 public:
+
     int leastInterval(vector<char>& tasks, int n) {
-        int intervals = 0;
-        unordered_map<char,int> mp;
-        priority_queue<int> q;
-        for(auto c: tasks){
-            mp[c]++;
-        }
-
-        for(auto c: mp){
-            q.push(c.second);
-        }
-
-        while(!q.empty()){
-            vector<int> temp;
-            int i = 0;
-
-            while(i <= n){
-                if(!q.empty()){
-                    int count = q.top();
-                    q.pop();
-                    if(count > 1)
-                        {
-                            temp.push_back(count-1);
-                            }
-                    
-                }
-                intervals++;
-                if(q.empty() && temp.empty()) break;
-                i++;
+        vector<int> mp(26,0);
+        int max_freq=0, count_maxfreq=0, sz=tasks.size();
+        for(char i:tasks){
+            mp[i-'A']++; 
+            if(mp[i-'A']>max_freq){
+                max_freq=mp[i-'A'];
             }
-            for(int t: temp){
-                q.push(t);
-            }
+        }   
+        for(int i=0;i<26;i++){
+            if(mp[i]==max_freq) count_maxfreq++; 
         }
-        return intervals;
-        
+        int time= (max_freq-1)*(n+1)+count_maxfreq; 
+        return max(time,sz);
     }
 };
