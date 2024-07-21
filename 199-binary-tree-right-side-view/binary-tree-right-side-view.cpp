dbc5e076010cd267c7857a0b73176dcb98c22808
Sowmya Varakala <66997833+sowmyavarakala@users.noreply.github.com>
2024-07-21 17:13:38 -0400

    Time: 0 ms (100.00%) | Memory: 14.2 MB (49.45%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -14,13 +14,14 @@ public:
     vector<int> ans;
     void dfs(TreeNode* root, int level){
         if(!root) return;
-        if(ans.size() == level)
+        if(ans.size() == level){
             ans.push_back(root->val);
+        }
         dfs(root->right,level+1);
         dfs(root->left,level+1);
     }
     vector<int> rightSideView(TreeNode* root) {
-        dfs(root,0);
+        dfs(root, 0);
         return ans;
     }
 };
