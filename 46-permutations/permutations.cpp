65954bf64ac99307fad05a847d7a48f1c9994a3f
Sowmya Varakala <66997833+sowmyavarakala@users.noreply.github.com>
2024-07-26 21:57:53 -0400

    Time: 6 ms (17.81%) | Memory: 10.3 MB (18.76%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
-    vector<vector<int>> permute(vector<int> &nums) {
-        vector<int> path;
-        vector<bool> visited(nums.size(),false);
-
-        dfs(nums,path,visited);
-        return ans;
-    }
-    
-    void dfs(vector<int> &nums, vector<int> &path, vector<bool> &visited){
+    vector<vector<int>> result;
+    void helper(vector<int>& nums, vector<bool>& visited, vector<int>& path){
         if(path.size() == nums.size()){
-            ans.push_back(path);
+            result.push_back(path);
             return;
         }
-        for(int i = 0; i< nums.size(); i++){
-            if(visited[i] == false){
-                visited[i] = true;
+        for(int i = 0; i < nums.size(); i++){
+            if(!visited[i]){
                 path.push_back(nums[i]);
-                dfs(nums,path,visited);
-
-                path.pop_back();
+                visited[i] = true;
+                helper(nums, visited, path);
                 visited[i] = false;
+                path.pop_back();
             }
+            
         }
     }
-
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<bool> visited(nums.size(), false);
+        vector<int> path;
+        helper(nums, visited, path);
+        return result;
+    }
 };
