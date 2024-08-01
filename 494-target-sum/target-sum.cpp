3be15f3ab35dd0f085ac8a7597247984fdffa8a6
Sowmya Varakala <66997833+sowmyavarakala@users.noreply.github.com>
2024-07-31 21:41:26 -0400

    Time: 6 ms (75.77%) | Memory: 11.5 MB (65.74%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
-    int find(vector<int>& nums, int& target, int n, int curr){
-        if(n == 0){
-            if(target == curr){
-                return 1;
-            }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int sum = 0;
+        for(int i = 0; i < nums.size(); i++){
+            sum += nums[i];
+        }
+        if(sum - target < 0 || (sum - target) % 2 != 0){
             return 0;
         }
-        return find(nums, target, n - 1, curr + nums[n-1]) + find(nums, target, n - 1, curr - nums[n-1]);
-    }
-    int findTargetSumWays(vector<int>& nums, int target) {
-        return find(nums, target, nums.size(), 0);
+        target = (sum - target) / 2;
+
+        vector<int> dp(target + 1, 0);
+        dp[0] = 1;
+
+        // Fill the DP table
+        for (int num : nums) {
+            for (int j = target; j >= num; --j) {
+                dp[j] += dp[j - num];
+            }
+        }
+
+        return dp[target];
     }
 };
