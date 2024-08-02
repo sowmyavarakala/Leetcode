02d43c1393afca65ff243291506a89482fb8873e
Sowmya Varakala <66997833+sowmyavarakala@users.noreply.github.com>
2024-08-01 21:14:24 -0400

    Time: 48 ms (41.50%) | Memory: 51.2 MB (27.98%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -2,11 +2,9 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int maxReach = 0;
-        for (int i = 0; i < nums.size(); i++){
-            if(i > maxReach){
-                return false;
-            }
-            maxReach = max(maxReach, i + nums[i]);
+        for(int i = 0; i < nums.size(); i++){
+            if(i > maxReach) return false;
+            maxReach = max(maxReach, nums[i] + i);
             if(maxReach >= nums.size()-1){
                 return true;
             }
