dd9d8115e55b6fbdb31025d41a6cbc8ee1af1203
Sowmya Varakala <66997833+sowmyavarakala@users.noreply.github.com>
2024-10-24 20:32:27 -0400

    Time: 35 ms (94.42%) | Memory: 17.8 MB (92.72%) - LeetSync

--- 224-basic-calculator/basic-calculator.py
+++ 224-basic-calculator/basic-calculator.py
@@ -1,30 +1,28 @@
 class Solution:
     # using stack
     def calculate(self, s: str) -> int:
-        num = 0
-        sign = 1
-        res = 0
-        stack = []
+        def helper(i):
+            num = 0
+            sign = 1
+            res = 0
 
-        for i in range(len(s)):
-            c = s[i]
-            if c.isdigit():
-                num = num*10 + int(c)
-            elif c in '+-':
-                res += num * sign
-                sign = 1 if c is '+' else -1
-                num = 0
-            elif c == '(':
-                stack.append(res)
-                stack.append(sign)
-                sign = 1
-                res = 0
-            elif c == ')':
-                res += num * sign
-                res *= stack.pop()
-                res += stack.pop()
-                num = 0
+            while i < len(s):
+                c = s[i]
+                if c.isdigit():
+                    num = num*10 + int(c)
+                elif c in '+-':
+                    res += num * sign
+                    sign = 1 if c is '+' else -1
+                    num = 0
+                elif c == '(':
+                    num, i = helper(i + 1)
+                elif c == ')':
+                    res += num * sign
+                    return res, i
+                i += 1
         
-        return res + num * sign
+            return res + num * sign, i
+        result, _ = helper(0)
+        return result
 
         
