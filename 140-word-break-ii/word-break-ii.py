89128de01d3755562caa56fa1538ba240af728de
Sowmya Varakala <66997833+sowmyavarakala@users.noreply.github.com>
2024-10-30 17:42:58 -0400

    Time: 3 ms (7.17%) | Memory: 16.6 MB (27.20%) - LeetSync

---
+++ 140-word-break-ii/word-break-ii.py
@@ -0,0 +1,30 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        word_set = set(wordDict)
+        n = len(s)
+
+        dp = [[] for _ in range(n + 1)]
+        dp[0] = [""]
+
+        # Populate the DP table
+        for i in range(1, n + 1):
+            for j in range(i):
+                if dp[j] and s[j:i] in word_set:
+                    dp[i].append(s[j:i])
+
+        def backtrack(end):
+            if end == 0:
+                return [""]
+
+            sentences = []
+            for word in dp[end]:
+                for sentence in backtrack(end - len(word)):
+                    if sentence:
+                        sentences.append(sentence + " " + word)
+                    else:
+                        sentences.append(word)
+            return sentences
+
+        return backtrack(n)
+
+        
