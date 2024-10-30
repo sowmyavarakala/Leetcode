6542f313b46a14398ae76b11d1134e472903ad5f
Sowmya Varakala <66997833+sowmyavarakala@users.noreply.github.com>
2024-10-30 18:12:05 -0400

    Time: 0 ms (100.00%) | Memory: 16.5 MB (91.33%) - LeetSync

--- 140-word-break-ii/word-break-ii.py
+++ 140-word-break-ii/word-break-ii.py
@@ -1,35 +1,22 @@
-class Solution:
-    def __init__(self):
-        self.memo = {}  # Memoization cache
-
-    def solve(self, s, wordSet):
-        # If result for the current substring is already computed, return it
-        if s in self.memo:
-            return self.memo[s]
-        
-        # Base case: if the string is empty, return a list with an empty string
-        if not s:
-            return [""]
-
-        sentences = []  # Store all possible sentences from this point
-
-        # Try all possible prefixes of the string
-        for i in range(1, len(s) + 1):
-            prefix = s[:i]  # Get the prefix from 0 to i
-            if prefix in wordSet:  # Check if the prefix is a valid word
-                # Recurse for the remaining part of the string
-                remaining_sentences = self.solve(s[i:], wordSet)
-                # Combine the current prefix with each sentence from the remaining part
-                for sentence in remaining_sentences:
-                    if sentence:
-                        sentences.append(prefix + " " + sentence)
-                    else:
-                        sentences.append(prefix)
-
-        # Store the result in memo to avoid redundant computation
-        self.memo[s] = sentences
-        return sentences
-
+class Solution(object):
     def wordBreak(self, s, wordDict):
-        wordSet = set(wordDict)  # Convert wordDict to a set for O(1) lookups
-        return self.solve(s, wordSet)  # Start backtracking from the full string
+        return self.helper(s, wordDict, {})
+    
+    
+    def helper(self, s, wordDict, memo):
+        if s in memo: return memo[s]
+        if not s: return []
+        
+        res = []
+        for word in wordDict:
+            if not s.startswith(word):
+                continue
+            if len(word) == len(s):
+                res.append(word)
+            else:
+                resultOfTheRest = self.helper(s[len(word):], wordDict, memo)
+                for item in resultOfTheRest:
+                    item = word + ' ' + item
+                    res.append(item)
+        memo[s] = res
+        return res
