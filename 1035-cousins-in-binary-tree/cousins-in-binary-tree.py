e50d776e00bf0b776edb21e7f0d56364749d70c2
Sowmya Varakala <66997833+sowmyavarakala@users.noreply.github.com>
2024-11-12 16:03:01 -0500

    Time: 0 ms (100.00%) | Memory: 16.8 MB (14.95%) - LeetSync

--- 1035-cousins-in-binary-tree/cousins-in-binary-tree.py
+++ 1035-cousins-in-binary-tree/cousins-in-binary-tree.py
@@ -4,19 +4,28 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
+
 class Solution:
     def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool:
         res = []
-        def dfs(node, parent, depth):
-            if not node:
-                return
+
+        queue = deque([(root, None, 0)])
+
+        while queue:
+            if len(res) == 2:
+                break
+            node, parent, depth = queue.popleft()
             if node.val == x or node.val == y:
                 res.append((parent, depth))
-            dfs(node.left, node, depth + 1)
-            dfs(node.right, node, depth + 1)
+            
+            if node.left:
+                queue.append((node.left, node, depth + 1))
+            
+            if node.right:
+                queue.append((node.right, node, depth + 1))
         
-        dfs(root, None, 0)
-
         n1, n2 = res
 
         return n1[0] != n2[0] and n1[1] == n2[1]
+        
