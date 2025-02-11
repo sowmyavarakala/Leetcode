892452b8fdf18d801100ec909def8cfc1cff2a3f
Sowmya Varakala <66997833+sowmyavarakala@users.noreply.github.com>
2025-02-10 22:08:46 -0500

    Time: 3 ms (76.17%) | Memory: 19 MB (73.29%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,13 +1,20 @@
 class Solution:
-    def majorityElement(self, nums: List[int]) -> List[int]:
-        element_count = Counter(nums)
-
-        majority_elements = []
-        threshold = len(nums) // 3
-
-        for element, count in element_count.items():
-            if count > threshold:
-                majority_elements.append(element)
-        
-        return majority_elements
-        
+# @param {integer[]} nums
+# @return {integer[]}
+    def majorityElement(self, nums):
+        if not nums:
+            return []
+        count1, count2, candidate1, candidate2 = 0, 0, 0, 1
+        for n in nums:
+            if n == candidate1:
+                count1 += 1
+            elif n == candidate2:
+                count2 += 1
+            elif count1 == 0:
+                candidate1, count1 = n, 1
+            elif count2 == 0:
+                candidate2, count2 = n, 1
+            else:
+                count1, count2 = count1 - 1, count2 - 1
+        return [n for n in (candidate1, candidate2)
+                        if nums.count(n) > len(nums) // 3]
